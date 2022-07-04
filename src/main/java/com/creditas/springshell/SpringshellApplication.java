751ec2e7be340d829438cd65e7f1e0d24adc7842
Valdir Torres Borges <valdyrtorres@yahoo.com.br>
2022-07-04 13:54:08 -0300

    commit do exemplo

---
+++ src/main/java/com/creditas/springshell/SpringshellApplication.java
@@ -0,0 +1,13 @@
+package com.creditas.springshell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringshellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringshellApplication.class, args);
+	}
+
+}
